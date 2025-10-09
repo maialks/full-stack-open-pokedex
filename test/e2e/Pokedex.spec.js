ffefb1a3ced0b77365a4ce4d15cc917d345b06ea
maialks <lucasmss01ita@gmail.com>
2025-10-09 17:57:27 -0300

    added pokemon page navigation test

---
+++ test/e2e/Pokedex.spec.js
@@ -0,0 +1,20 @@
+const { test, describe, expect, beforeEach } = require('@playwright/test')
+
+describe('Pokedex', () => {
+  beforeEach(async ({ page }) => {
+    await page.goto('http://localhost:5000')
+  })
+  test('front page can be opened', async ({ page }) => {
+    await expect(page.getByText('ivysaur')).toBeVisible()
+    await expect(
+      page.getByText(
+        'Pokémon and Pokémon character names are trademarks of Nintendo.'
+      )
+    ).toBeVisible()
+  })
+
+  test('can navigate from main page to pokemon page', async ({ page }) => {
+    await page.getByRole('link', { name: 'charizard' }).click()
+    await expect(page.getByText('solar power')).toBeVisible()
+  })
+})
